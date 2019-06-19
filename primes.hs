1428ddc68efb580f72a6fe37be2fdd89a7eeedc3
dedSasha <dr.ddb2@yandex.ru>
2019-06-20 00:46:36 +0300

    primes

---
+++ primes.hs
@@ -0,0 +1,9 @@
+import Data.List
+
+is_prime :: Int -> Bool
+is_prime 1 = False
+is_prime 2 = True
+is_prime n | (length [x | x <- [2 .. n-1], mod n x == 0]) > 0 = False
+		   | otherwise = True
+
+primes = [x | x <- [1..], isPrime x]
