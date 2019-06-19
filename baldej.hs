18ff0c2b8066691048d1845d13d753ab1c76f845
dedSasha <dr.ddb2@yandex.ru>
2019-06-19 18:57:21 +0300

    i love Haskell
    baldej.hs

---
+++ baldej.hs
@@ -0,0 +1,13 @@
+import Data.List
+import Data.Numbers.Primes
+l = [1, 2, 3, 4, 5, 6, 0, 0]
+ml = map (\x -> x^2) l
+fl = filter (\x -> if x==0 then True else False)
+--ща самый балдёж
+fucking_cool_l = [x^2 | x <- [1, 3..]]
+--не ну это BaLdEj
+--шо я увидел
+super_fucking_cool_l = [(x, y, i) | x <- [1..100],
+                                    y <- [1..100],
+                                    i <- [1..100],
+                                    x^2 + y^2 == i^2]
