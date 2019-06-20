21ea3d10d3d6b912bd9aa091d50c08ae7f974c29
dedSasha <dr.ddb2@yandex.ru>
2019-06-20 10:50:28 +0300

    fix(please don't hit me)

--- baldej.hs
+++ baldej.hs
@@ -1,7 +1,7 @@
 import Data.List
 l = [1, 2, 3, 4, 5, 6, 0, 0]
 ml = map (\x -> x^2) l
-fl = filter (\x -> if x==0 then True else False)
+fl = filter (\x -> if x==0 then True else False) l
 --ща самый балдёж
 fucking_cool_l = [x^2 | x <- [1, 3..]]
 --не ну это BaLdEj
@@ -9,4 +9,4 @@ fucking_cool_l = [x^2 | x <- [1, 3..]]
 super_fucking_cool_l = [(x, y, i) | x <- [1..100],
                                     y <- [1..100],
                                     i <- [1..100],
-                                    ^2 + y^2 == i^2]
+                                    x^2 + y^2 == i^2]
